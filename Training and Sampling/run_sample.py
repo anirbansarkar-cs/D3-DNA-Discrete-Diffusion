2a4f4b64cf2921f56186e77ec32e4ef051d2bb2f
Anirban Sarkar <anirban.mca2007@gmail.com>
2024-06-10 15:50:28 -0400

    Update run_sample.py

--- Training and Sampling/run_sample.py
+++ Training and Sampling/run_sample.py
@@ -10,21 +10,23 @@ import h5py, os
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset, DistributedSampler
 
-from PL_DeepSTARR import *
+from PL_DeepSTARR import * #Required for DeepSTARR
+from PL_mpra import * #Required for MPRA
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate some samples")
     parser.add_argument("--model_path", default="", type=str) #Need to provide model path
     parser.add_argument("--batch_size", type=int, default=256)
-    parser.add_argument("--steps", type=int, default=249) #Sequence length
+    parser.add_argument("--steps", type=int, default=249) #Sequence length #Change default to 200 for MPRA
     args = parser.parse_args()
 
     
     device = torch.device('cuda')
     model, graph, noise = load_model_local(args.model_path, device)
 
-    #Deepstarr
+    #DeepSTARR. Comment the below lines and uncomment the following ones for MPRA
+    ##########
     filepath = os.path.join('DeepSTARR_data.h5') #load DeepSTARR data
     data = h5py.File(filepath, 'r')
     ckpt_aug_path = os.path.join('oracle_DeepSTARR_DeepSTARR_data.ckpt') #Load DeepSTARR oracle model
@@ -42,6 +44,28 @@ def main():
     sampling_fn = sampling.get_pc_sampler(
             graph, noise, (batch.shape[0], 249), 'analytic', args.steps, device=device
         )
+    ##########
+
+    #MPRA
+    #########
+    # filepath = os.path.join('mpra_data.h5') #load DeepSTARR data
+    # data = h5py.File(filepath, 'r')
+    # ckpt_aug_path = os.path.join('oracle_mpra_mpra_data.ckpt') #Load MPRA oracle model
+
+    # #We select test data to calculate MSE and generate samples. Change if required
+    # X_test = torch.tensor(np.array(data['x_test']).astype(np.float32)).permute(0,2,1)
+    # y_test = torch.tensor(np.array(data['y_test']).astype(np.float32))
+    # X_test = torch.argmax(X_test, dim=1)
+    # testing_ds = TensorDataset(X_test, y_test)
+    # test_ds = torch.utils.data.DataLoader(testing_ds, batch_size=args.batch_size, shuffle=False,
+    #                                          num_workers=4)
+    # mpra = PL_mpra.load_from_checkpoint(ckpt_aug_path, input_h5_file=filepath).eval()
+
+    # val_pred_seq = []
+    # sampling_fn = sampling.get_pc_sampler(
+    #         graph, noise, (batch.shape[0], 200), 'analytic', args.steps, device=device
+    #     )
+    #########
     
     for _, (batch, val_target) in enumerate(test_ds):
         sample = sampling_fn(model, val_target.to(device))
@@ -49,8 +73,15 @@ def main():
         val_pred_seq.append(seq_pred_one_hot)
 
     val_pred_seqs = torch.cat(val_pred_seq, dim=0)
+    
+    #Below two lines are for DeepSTARR
     val_score = deepstarr.predict_custom(deepstarr.X_test.to(device))
     val_pred_score = deepstarr.predict_custom(val_pred_seqs.permute(0, 2, 1).to(device))
+
+    #Below two lines are for MPRA
+    # val_score = mpra.predict_custom(mpra.X_test.to(device))
+    # val_pred_score = mpra.predict_custom(val_pred_seqs.permute(0, 2, 1).to(device))
+    
     sp_mse = (val_score - val_pred_score) ** 2
     mean_sp_mse = torch.mean(sp_mse).cpu()
     print(f"Test-sp-mse {mean_sp_mse}")
