a7fcd8752849424a979fa65a14060b8785fdf4fc
Anirban Sarkar <anirban.mca2007@gmail.com>
2024-06-10 13:45:06 -0400

    Update data.py

--- Training and Sampling/data.py
+++ Training and Sampling/data.py
@@ -12,9 +12,10 @@ import h5py, os
 from datasets import Dataset
 
 '''
-Please follow codes from [Dirichlet-flow-matching](https://github.com/HannesStark/dirichlet-flow-matching) and [Dirichlet diffusion score model](https://github.com/jzhoulab/ddsm) for setting up the code to train for Promoter dataset.
+Please follow codes from "Dirichlet-flow-matching" (https://github.com/HannesStark/dirichlet-flow-matching) and "Dirichlet diffusion score model" (https://github.com/jzhoulab/ddsm)
+for setting up the code to train for Promoter dataset and run import by uncommenting below line.
 '''
-from promoter_dataset import PromoterDataset
+# from promoter_dataset import PromoterDataset
 
 from torch.utils.data import DataLoader, TensorDataset, DistributedSampler
 
