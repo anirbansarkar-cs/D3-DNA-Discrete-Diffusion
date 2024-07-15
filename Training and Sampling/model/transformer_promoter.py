4db0a3b2e3b01f5e518e26e855e567e3cd2dbe1c
Anirban Sarkar <anirban.mca2007@gmail.com>
2024-07-15 15:21:57 -0400

    Update transformer_promoter.py

--- Training and Sampling/model/transformer_promoter.py
+++ Training and Sampling/model/transformer_promoter.py
@@ -242,6 +242,22 @@ class DDitFinalLayer(nn.Module):
         return x
 
 
+class GaussianFourierProjection(nn.Module):
+    """
+    Gaussian random features for encoding time steps.
+    """
+
+    def __init__(self, embed_dim, scale=30.):
+        super().__init__()
+        # Randomly sample weights during initialization. These weights are fixed
+        # during optimization and are not trainable.
+        self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
+
+    def forward(self, x):
+        x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
+        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
+
 class Dense(nn.Module):
     """
     A fully connected layer that reshapes outputs to feature maps.
